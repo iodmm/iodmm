a55b6d5b5aaf51e1cc5eb47464f2a38c24e549db
iodmm <140909710+iodmm@users.noreply.github.com>
2023-08-07 08:59:17 +0000

    yes

---
+++ LibGen.c
@@ -0,0 +1,57 @@
+// LIBRERIA ESTUDIO LENGUAJE C
+#include <stdio.h>
+
+int main()/*Indexación*/
+{
+    
+    char word[10] = "dump";
+
+    word[1] = 'a';
+    puts(word);
+
+    // NO OLVIDAR
+    // apóstrofes: 'a' char variable
+    // comillas: "dump" char * array
+
+    /*Nos queda claro que la aparición del nombre del
+    arreglo seguido de un par de corchetes significa
+    que queremos acceder (para obtener o modificar)
+    solo a uno de los elementos del arreglo , el que
+    tiene el índice igual al valor calculado de la
+    expresión. encerrado entre paréntesis.*/
+
+    /*¡Estos conocidos corchetes [ y ] no son más que
+    un operador! Este operador tiene la prioridad más
+    alta posible: es tan alta como la prioridad del
+    sufijo ++y --los operadores.*/
+
+    /*El estándar de lenguaje "C" dice: si cualquier
+    puntero es seguido por un operador de indexación,
+    como este*/
+
+    // t[i]
+    //*(t+i)
+
+    /*Esto significa que la expresión involucra dos
+    argumentos*/
+
+    // word[1]
+    /*word se interpreta como puntero al primer elemento
+    de la matriz*/
+    // El puntero se incrementa en 1 word + 1
+    //*(word + 1 ) = 'a'; esta asignacion es totalmente admisible
+    //Es obligatorio utilizar los parentesis por las prioridades de los operadores
+    //El operador [] es conmutativo
+    //Al compilador le da igual word[1] q 1[word]
+    //Precaucion al contruir expresiones con este operador
+
+    char string[]="ABC";
+    char *p;
+    char c;
+
+    //Configuramos p que apunte a la segunda posicion 
+
+    //La forma recomendada es:
+    p = string + 1;
+    return 0;
+}
