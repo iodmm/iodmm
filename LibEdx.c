95c4f1a0998a4688c6d64a1f209e318f711169a3
iodmm <140909710+iodmm@users.noreply.github.com>
2023-08-07 07:59:17 +0000

    yes

--- LibEdx.c
+++ LibEdx.c
@@ -1,15 +1,14 @@
-/*Plantilla inicial
-#include <stdio.h> // Instrucción de preprocesamiento
+/*Plantilla inicial*/
+ #include <stdio.h> // Instrucción de preprocesamiento
 
 //int tipo de valor que devuelve la funcion
 //main nombre de la funcion
 //()dentro de parentesis, los parametros de la funcion
-int main() // Función main, funcion principal
-{
+int main(){// Función main, funcion principal
+
     //Cuerpo de la funcion
     //instruccion printf imprime en pantalla
     printf("¡Hola, mundo!");
     //instruccion return devuelve numero entero 0
     return 0;
 }
-*/
